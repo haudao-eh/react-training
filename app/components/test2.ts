7275bc2c31056a9b6ff468864af39a51d7517821
Hau Dao <hau.dao@employmenthero.com>
2024-01-04 14:34:34 +0700

    t21

--- app/components/test2.ts
+++ app/components/test2.ts
@@ -219,7 +219,7 @@ const AdvancedSearchSettings = ({
             <Button
               variant="text"
               icon="restart"
-              text="Resettt"
+              text="Resett"
               onClick={() => setAdvancedSearchValues(initialFilterValues)}
             />
             <Button
