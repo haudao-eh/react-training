610f614e34516c2acb8c5823f4d26490ebb907b0
Hau Dao <hau.dao@employmenthero.com>
2024-01-12 12:23:39 +0700

    t

--- app/components/test2.ts
+++ app/components/test2.ts
@@ -86,7 +86,7 @@ const AdvancedSearchSettings = ({
             pr: 'medium',
             pt: 'small',
             pb: 'small',
-            height: 80,
+            height: 90,
           }}
         >
           <Grid sx={{ mb: 'medium' }}>
