007c042a842520ed53eab319ec12a2a566b514b8
Hau Dao <hau.dao@employmenthero.com>
2024-03-27 15:45:44 +0700

    Add test bug file

---
+++ app/components/TestBug.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button, Typography, Box, ButtonProps } from '@hero-design/react';
+
+import emptyBanner from './assets/empty-banner.png';
+
+const EmptyImg = styled.img`
+  height: 172px;
+  margin-bottom: ${({ theme }) => theme.space.small}px;
+`;
+
+interface EmptyTableViewProps {
+  message: string;
+  buttonText: ButtonProps['text'];
+  buttonIcon?: ButtonProps['icon'];
+  onButtonClick: ButtonProps['onClick'];
+}
+
+const EmptyTableView = ({
+  message,
+  buttonText,
+  buttonIcon = 'moneybag-outlined',
+  onButtonClick,
+}: EmptyTableViewProps) => (
+  <Box sx={{ textAlign: 'center' }}>
+    <EmptyImg src={emptyBanner} alt="Empty" />
+    <Typography.Text
+      fontSize={14}
+      sx={{ color: 'bodyText', mb: 'xxlarge', whiteSpace: 'pre-wrap' }}
+    >
+      {message}
+    </Typography.Text>
+
+    <Button
+      icon={buttonIcon}
+      onClick={onButtonClick}
+      sx={{
+        fontWeight: 'semi-bold',
+      }}
+      text={buttonText}
+    />
+  </Box>
+);
+
+export default EmptyTableView;
