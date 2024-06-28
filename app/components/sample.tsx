9a77e6407d8ea4c8b78c59cb7450137221f6cbd6
Hau Dao <hau.dao@employmenthero.com>
2024-06-28 10:01:51 +0700

    tes

--- app/components/sample.tsx
+++ app/components/sample.tsx
@@ -27,7 +27,7 @@ const Sample = () => {
     <Card style={{ width: 200 }} /> // acceptable
     // Snowflakes using sx prop
     <Badge sx={{ mt: 10 }} /> // acceptable
-    <Badge sx={{ pt: 10 }} />
+    <Badge sx={{ pt: 20 }} />
   </>;
 };
 
